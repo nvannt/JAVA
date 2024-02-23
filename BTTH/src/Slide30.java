0f9f37fd72fae300316da88393da93bb21e8d08c
nvannt <ngayaythatdep2512@gmail.com>
2024-02-23 21:21:30 +0700

    Slide30

---
+++ BTTH/src/Slide30.java
@@ -0,0 +1,16 @@
+import java.util.Scanner; 
+public class Slide30 {
+    public static void main(String [] args) {
+        int n, soDu ,tong = 0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Nhap vao so nguyen duong bat ki: ");
+        n = sc.nextInt();
+         while (n > 0) {
+            soDu = n % 10;
+            n = n /10;
+            tong += soDu;
+         }
+        System.out.println("Tong cac chu so = " + tong) ;
+        sc.close();
+    }
+}
